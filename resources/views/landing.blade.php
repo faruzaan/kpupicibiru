3b6f26ef32c0f7e78c53ee6f9bc6bbac820537f2
TitamSeptian <dwiputra4456@protonmail.com>
2021-12-07 13:50:41 +0700

    add landing page

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,216 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/animate/animate.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <title>Hello, world!</title>
+</head>
+
+<body>
+    <div class="background">
+        <div class="bulet1 wow bounceInUp" data-wow-duration="2s" data-wow-delay="0.5s">
+            <svg width="1600" height="1600" viewBox="0 0 2107 2107" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g filter="url(#filter0_d_61_3)">
+                    <circle cx="1053.5" cy="1053.5" r="798.5" fill="white" />
+                </g>
+                <defs>
+                    <filter id="filter0_d_61_3" x="0" y="0" width="2107" height="2107" filterUnits="userSpaceOnUse"
+                        color-interpolation-filters="sRGB">
+                        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                            result="hardAlpha" />
+                        <feMorphology radius="5" operator="dilate" in="SourceAlpha" result="effect1_dropShadow_61_3" />
+                        <feOffset />
+                        <feGaussianBlur stdDeviation="125" />
+                        <feComposite in2="hardAlpha" operator="out" />
+                        <feColorMatrix type="matrix"
+                            values="0 0 0 0 0.984314 0 0 0 0 0.866667 0 0 0 0 0 0 0 0 0.25 0" />
+                        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_61_3" />
+                        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_61_3" result="shape" />
+                    </filter>
+                </defs>
+            </svg>
+        </div>
+        <div class="bulet2 wow bounceInUp" data-wow-duration="2s" data-wow-delay="0.5s">
+            <svg width="1358" height="1358" viewBox="0 0 1358 1358" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g filter="url(#filter0_d_61_5)">
+                    <circle cx="679" cy="679" r="424" fill="white" />
+                </g>
+                <defs>
+                    <filter id="filter0_d_61_5" x="0" y="0" width="1358" height="1358" filterUnits="userSpaceOnUse"
+                        color-interpolation-filters="sRGB">
+                        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                            result="hardAlpha" />
+                        <feMorphology radius="5" operator="dilate" in="SourceAlpha" result="effect1_dropShadow_61_5" />
+                        <feOffset />
+                        <feGaussianBlur stdDeviation="125" />
+                        <feComposite in2="hardAlpha" operator="out" />
+                        <feColorMatrix type="matrix"
+                            values="0 0 0 0 0.984314 0 0 0 0 0.866667 0 0 0 0 0 0 0 0 0.25 0" />
+                        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_61_5" />
+                        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_61_5" result="shape" />
+                    </filter>
+                </defs>
+            </svg>
+
+        </div>
+        <div class="bulet3 wow bounceInUp" data-wow-duration="2s" data-wow-delay="0.5s">
+            <svg width="1358" height="1358" viewBox="0 0 1358 1358" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g filter="url(#filter0_d_61_5)">
+                    <circle cx="679" cy="679" r="424" fill="white" />
+                </g>
+                <defs>
+                    <filter id="filter0_d_61_5" x="0" y="0" width="1358" height="1358" filterUnits="userSpaceOnUse"
+                        color-interpolation-filters="sRGB">
+                        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                            result="hardAlpha" />
+                        <feMorphology radius="5" operator="dilate" in="SourceAlpha" result="effect1_dropShadow_61_5" />
+                        <feOffset />
+                        <feGaussianBlur stdDeviation="125" />
+                        <feComposite in2="hardAlpha" operator="out" />
+                        <feColorMatrix type="matrix"
+                            values="0 0 0 0 0.984314 0 0 0 0 0.866667 0 0 0 0 0 0 0 0 0.25 0" />
+                        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_61_5" />
+                        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_61_5" result="shape" />
+                    </filter>
+                </defs>
+            </svg>
+
+        </div>
+    </div>
+    <div class="content">
+        <div class="container-fluid px-5">
+            {{-- header --}}
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="d-flex flex-column justify-content-center vh-100">
+                        <div class="img-3">
+                            <img class="img-fluid" src="{{ asset('img/upi-logo.png') }}" alt="Logo UPI">
+                            <img class="img-fluid" src="{{ asset('img/bem-logo.png') }}" alt="Logo BEM">
+                            <img class="img-fluid" src="{{ asset('img/kpu-logo.png') }}" alt="Logo KPU">
+                        </div>
+                        <h1 class="text-color-1 font-weight-bold">SIPDA</h1>
+                        <h1 class="text-color-1">UPI CIBIRU</h1>
+                        <p class="text-color-1 hero-desc font-weight-normal">Lörem ipsum öbiliga prehöras, anak
+                            kanskeman pojel pseudos
+                            peligen för
+                            att kosogt. Mivulig
+                            nist om än maskap trebiras, mikrohet fonoosmos.</p>
+                        <button class="btn btn-color-1 w-50 p-3 my-rounded">Gunakan Hak Pilih Sekarang !</button>
+                    </div>
+                </div>
+                {{-- <div class="col-md-2"></div> --}}
+                <div class=" col-md-5 offset-1">
+                    <div class="d-flex flex-column justify-content-center vh-100">
+                        <div class="embed-responsive embed-responsive-16by9 hero-video">
+                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0"
+                                allowfullscreen></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {{-- squer --}}
+            <div class="bg-white text-dark mx-auto shadow-lg my-rounded w-75">
+                <div class="row">
+                    <div class="col-md-4 py-3">
+                        <div class="text-center">
+                            <p>Suara Masuk Hari ini</p>
+                            <h5>99</h5>
+                        </div>
+                    </div>
+                    <div class="col-md-4 py-3">
+                        <div class="text-center">
+                            <p>Mahasiswa Aktif</p>
+                            <h5>99</h5>
+                        </div>
+                    </div>
+                    <div class="col-md-4 py-3">
+                        <div class="text-center">
+                            <p>Total Suara Masuk</p>
+                            <h5>99</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <br>
+            <p class="text-center mt-5">PASLON</p>
+            <br>
+            <br>
+            <br>
+            <div class="paslon">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="paslon-card shadow-lg">
+                            <div class="paslon-card-title">PASLON 1</div>
+                            <div class="paslon-card-body">
+                                <img src="{{ asset('img/paslon/paslon1.png') }}" alt="foto paslon"
+                                    class="img-fluid">
+                            </div>
+                            <div class="paslon-name">
+                                <p>Alif & Iman</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="paslon-card shadow-lg">
+                            <div class="paslon-card-title">PASLON 1</div>
+                            <div class="paslon-card-body">
+                                <img src="{{ asset('img/paslon/paslon1.png') }}" alt="foto paslon"
+                                    class="img-fluid">
+                            </div>
+                            <div class="paslon-name">
+                                <p>Alif & Iman</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="paslon-card shadow-lg">
+                            <div class="paslon-card-title">PASLON 1</div>
+                            <div class="paslon-card-body">
+                                <img src="{{ asset('img/paslon/paslon1.png') }}" alt="foto paslon"
+                                    class="img-fluid">
+                            </div>
+                            <div class="paslon-name">
+                                <p>Alif & Iman</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <footer class="footer py-5 mt-5">
+                <h1>For any inquiries please hit us up through</h1>
+                <div class="social">
+                    <svg width="29" height="29" viewBox="0 0 61 61" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M51.8636 8.86406C46.1585 3.14531 38.5607 0 30.4864 0C13.8203 0 0.258705 13.5616 0.258705 30.2277C0.258705 35.5516 1.64754 40.7529 4.28906 45.3415L0 61L16.0261 56.7926C20.4377 59.2027 25.4076 60.469 30.4728 60.469H30.4864C47.1388 60.469 61 46.9074 61 30.2413C61 22.167 57.5687 14.5828 51.8636 8.86406ZM30.4864 55.3766C25.9658 55.3766 21.5406 54.1647 17.6873 51.8772L16.775 51.3326L7.27098 53.8243L9.80357 44.5518L9.20446 43.5987C6.68549 39.5955 5.36473 34.9797 5.36473 30.2277C5.36473 16.3801 16.6388 5.10603 30.5 5.10603C37.2127 5.10603 43.517 7.72031 48.2554 12.4723C52.9938 17.2243 55.9076 23.5286 55.894 30.2413C55.894 44.1025 44.3339 55.3766 30.4864 55.3766ZM44.2659 36.5592C43.517 36.1779 39.7998 34.3533 39.1054 34.1083C38.4109 33.8496 37.9071 33.727 37.4034 34.4895C36.8996 35.252 35.4563 36.9404 35.0069 37.4578C34.5712 37.9616 34.1219 38.0297 33.373 37.6484C28.9342 35.429 26.0203 33.6862 23.0929 28.6618C22.3167 27.3275 23.869 27.4228 25.3123 24.5362C25.5574 24.0324 25.4348 23.5967 25.2442 23.2154C25.0536 22.8342 23.5422 19.117 22.9158 17.6056C22.3031 16.135 21.6768 16.3393 21.2138 16.3121C20.7781 16.2848 20.2743 16.2848 19.7705 16.2848C19.2667 16.2848 18.4498 16.4754 17.7554 17.2243C17.0609 17.9868 15.1138 19.8114 15.1138 23.5286C15.1138 27.2458 17.8234 30.8404 18.1911 31.3442C18.5723 31.848 23.515 39.473 31.0991 42.7545C35.892 44.8241 37.771 45.0011 40.1674 44.6471C41.6243 44.4292 44.6335 42.8225 45.2598 41.0525C45.8862 39.2824 45.8862 37.771 45.6955 37.4578C45.5185 37.1174 45.0147 36.9268 44.2659 36.5592Z"
+                            fill="black" />
+                    </svg>
+
+                    <svg width="29" height="29" viewBox="0 0 62 62" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M31.0069 15.104C22.21 15.104 15.1144 22.2012 15.1144 31C15.1144 39.7988 22.21 46.896 31.0069 46.896C39.8038 46.896 46.8994 39.7988 46.8994 31C46.8994 22.2012 39.8038 15.104 31.0069 15.104ZM31.0069 41.3345C25.3221 41.3345 20.6747 36.6999 20.6747 31C20.6747 25.3001 25.3083 20.6655 31.0069 20.6655C36.7055 20.6655 41.3391 25.3001 41.3391 31C41.3391 36.6999 36.6917 41.3345 31.0069 41.3345ZM51.2563 14.4538C51.2563 16.5151 49.5965 18.1614 47.5495 18.1614C45.4886 18.1614 43.8426 16.5013 43.8426 14.4538C43.8426 12.4062 45.5024 10.7461 47.5495 10.7461C49.5965 10.7461 51.2563 12.4062 51.2563 14.4538ZM61.7822 18.2168C61.547 13.2501 60.4128 8.85072 56.7751 5.22604C53.1513 1.60136 48.7528 0.46692 43.7873 0.217896C38.6696 -0.0726319 23.3304 -0.0726319 18.2127 0.217896C13.261 0.453085 8.86258 1.58753 5.22487 5.21221C1.58717 8.83689 0.466815 13.2363 0.217847 18.2029C-0.0726157 23.3218 -0.0726157 38.6644 0.217847 43.7832C0.452984 48.7499 1.58717 53.1493 5.22487 56.774C8.86258 60.3986 13.2472 61.5331 18.2127 61.7821C23.3304 62.0726 38.6696 62.0726 43.7873 61.7821C48.7528 61.5469 53.1513 60.4125 56.7751 56.774C60.399 53.1493 61.5332 48.7499 61.7822 43.7832C62.0726 38.6644 62.0726 23.3356 61.7822 18.2168ZM55.1707 49.2756C54.0918 51.9872 52.0032 54.0762 49.2784 55.1691C45.1981 56.7878 35.516 56.4143 31.0069 56.4143C26.4978 56.4143 16.8019 56.774 12.7354 55.1691C10.0244 54.09 7.93586 52.001 6.84317 49.2756C5.22487 45.1944 5.59833 35.5101 5.59833 31C5.59833 26.4899 5.23871 16.7918 6.84317 12.7244C7.92203 10.0128 10.0106 7.9238 12.7354 6.83086C16.8157 5.21221 26.4978 5.58574 31.0069 5.58574C35.516 5.58574 45.2119 5.22604 49.2784 6.83086C51.9894 7.90996 54.078 9.999 55.1707 12.7244C56.789 16.8056 56.4155 26.4899 56.4155 31C56.4155 35.5101 56.789 45.2082 55.1707 49.2756Z"
+                            fill="black" />
+                    </svg>
+                </div>
+                <h5 class="text-right">Created by © KPU UPI CIBIRU. All rights reserved</h5>
+            </footer>
+        </div>
+
+    </div>
+
+    <script src="{{ asset('assets/vendor/jquery/jquery-3.5.1.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/wow/wow.min.js') }}"></script>
+    <script src="{{ asset('assets/js/script.js') }}"></script>
+</body>
+
+</html>
